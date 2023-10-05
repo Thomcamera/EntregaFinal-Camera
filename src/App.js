c23458c3249ddb203cf2458550a7ade24a33d9bc
Thomcamera <thomasigcamera@gmail.com>
2023-10-04 21:39:26 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar/NavBar.js";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.js";
+import "bulma/css/bulma.css"; 
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting={'Bienvenidos'}/>}/>
+          <Route path="/category/:categoryId" element={<ItemListContainer />}/>
+          <Route path="/item/:itemId" element={ <ItemDetailContainer/> }/>
+          <Route path="*" element={<h1>404 NOT FOUND</h1>}/>
+        </Routes>      
+      </BrowserRouter>     
+
     </div>
+    
   );
 }
 
